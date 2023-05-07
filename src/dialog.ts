269a5507a5bbe96846e47e398abbdc8e770a746a
Muhtalip Dede <extmuhtalipd@ext.d-teknoloji.com.tr>
2023-05-07 18:36:54 +0300

    add new dialogs

--- src/dialog.ts
+++ src/dialog.ts
@@ -4,7 +4,7 @@ export class Dialog {
 
     generate() {
         const fileHelper = new FileHelper();
-        const randomNumber = Math.floor(Math.random() * 5);
+        const randomNumber = Math.floor(Math.random() * 15);
         const randomLine = fileHelper.read(randomNumber);
         console.log(randomLine);
     }
