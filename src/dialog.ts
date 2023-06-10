f974bb98f0ebb2a83cba4789e4d862d49b0e3def
Muhtalip Dede <muhtalipdede@gmail.com>
2023-06-10 16:27:58 +0300

    @clack/prompts

--- src/dialog.ts
+++ src/dialog.ts
@@ -1,11 +1,29 @@
+import { isCancel, outro, select, spinner } from "@clack/prompts";
 import { FileHelper } from "./file-helper";
+import { setTimeout as sleep } from 'node:timers/promises';
 
 export class Dialog {
+    async generate() {
+        const type = await select({
+            message: 'İlkkan sen misin?',
+            options: [
+                { value: 'yilmaz', label: 'Yılmaz' },
+                { value: 'ilkkan', label: 'İlkkan' },
+                { value: 'ersoy', label: 'Ersoy' },
+            ],
+        });
 
-    generate() {
+        if(isCancel(type)) {
+            outro("Görüşürüz!");
+            return;
+        }
+        
+        const s = spinner();
+        s.start("Geliyor..." + type.toString());
+        await sleep(3000);
         const fileHelper = new FileHelper();
         const randomNumber = Math.floor(Math.random() * fileHelper.length());
         const randomLine = fileHelper.read(randomNumber);
-        console.log(randomLine);
+        s.stop(randomLine);
     }
 }
